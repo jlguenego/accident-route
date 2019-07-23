807d88470c7c37c0c58e5cccc6a6977b9383fb7b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-23 14:38:35 +0200

    add server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const serveIndex = require('serve-index');
+const app = express();
+
+app.use(express.static('.'));
+app.use(serveIndex('.', {'icons': true}));
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
