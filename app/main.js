e4cbb4a64659090bd23a21fff8c9a37092f4f439
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-23 15:16:30 +0200

    csv to bar

--- app/main.js
+++ app/main.js
@@ -4,6 +4,11 @@ console.log('hello', d3);
     try {
         const data = await d3.csv('data.csv')
         console.log('data', data);
+
+        const bars = document.querySelectorAll('.bar');
+        bars.forEach((bar, i) => {
+            bar.style.width = `${100 * data[i]['nbr-accident']/5000}%`;
+        });
     } catch (error) {
         console.log('error', error);
     }
